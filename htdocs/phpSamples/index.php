ecaa876a53033fdbc5fa8679f6c52e802e8d1f65
Roboy8051 <143159013+Roboy8051@users.noreply.github.com>
2023-08-25 14:18:49 +0900

    Add files via upload

---
+++ htdocs/phpSamples/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>hellon-wed</title>
+</head>
+
+<body>
+<h1>どうも皆さんこんにちは</h1>
+<h1>[~hellon~/~ハロン~]です。</h1>
+<p>このサイトは、PHPで出来ています</p>
+<p>Let`s PHP--
+<?php
+    print"Hello World!";
+?>
+</p>
+<p>
+100000-88888+5555は、<?=100000-88888+5555?>だよ!!(いらん情報)
+</p>
+<p><input type="submit" value="意味のないボタン"></p>
+<p>
+意味のないチェックボックス
+<input type="checkbox" name="iminonaibotann" value="1">本当に意味のないチェックボックス
+<input type="checkbox" name="iminonaibotann" value="2">こっちも意味のないチェックボックス
+</p>
+<?php
+    $a = 666; $b = 999;
+    print($a + $b);
+?>
+<p>書く事があんまりないんだけどどうすればいいのか分からん</p>
+<p>まあ、とりあえず意味のないボタンを、大量の意味のないボタンを置いときます</p>
+<p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p>
+<p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p>
+<p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p>
+<p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p>
+<p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p><p><input type="submit" value="意味のないボタン"></p>
+<p>
+<a href="http://hellon.php.xdomain.jp/syouyu/">
+=>醬油のウェブサイト<=(仲間＆~ハロン~(僕)のサーバーを、使っている)</a>
+</p>
+</body>
+</html>
---
+++ htdocs/phpSamples2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>hellon-wed-on-syouyu</title>
+</head>
+
+<body>
+<h1>どうも皆さんこんにちは</h1>
+<h1>[~syouyu~/~醬油~]です。</h1>
+<p>このサイトは、PHPで出来ています</p>
+<p>Let`s PHP--
+<?php
+    print"Hello World!";
+?>
+</p>
+<p><input type="submit" value="意味のないボタン"></p>
+<p>
+<a href="http://hellon.php.xdomain.jp/">
+=>ハロンのウェブサイト<=</a>
+</p>
